693d8803ef3e83bc4b6f1f976baae52a9c5d401e
Anthony E <e4nthony@gmail.com>
2021-11-14 21:54:04 +0200

    Minor Changes

--- app/src/main/java/com/example/hw1_tictactoe/MainActivity.java
+++ app/src/main/java/com/example/hw1_tictactoe/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.hw1_tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        setContentView(R.layout.game_field); // for testing todo: delete
+        //------------------------------------
+
+        // TODO: 14.11.2021 add ids of elements
     }
 }
